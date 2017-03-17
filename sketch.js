4f626fe02d9640425c085417b71ec0dec044d66a
lozgreeen <laurenmgreen1996@gmail.com>
2017-03-17 14:30:38 +0000

    Add files via upload
    jaap did it :)

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(300,400);
+  background(219,101,5);
+  noStroke();
+  fill(251,179,7);
+  rect(25,25,250,125);
+  noStroke();
+  fill(216,32,4);
+  rect(25,170,250,200);
+}
+
+function draw() {
+  noStroke();
+  fill(250,200,200,50);
+  rect(mouseX,mouseY,25,25);
+
+
+}
+
+function mousePressed() {
+  createCanvas(300,400);
+  background(219,101,5);
+  noStroke();
+  fill(251,179,7);
+  rect(25,25,250,125);
+  noStroke();
+  fill(216,32,4);
+  rect(25,170,250,200);
+  
+}
